a2503d787487085a9b876926156ab32404f56dfd
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-06-08 12:12:52 +0530

    typingapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faker} from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '3.typingApp';
+  randomText = faker.lorem.sentence(5);
+  inputText="";
+  takeInputData(event:any){
+    this.inputText=event.target.value;
+  }
+  compare(randomLetter:string, inputLetter:string){
+    return !inputLetter ? "pending" : inputLetter===randomLetter ? "correct" : "incorrect" ;
+  }
+
 }
